70428c6a21dd8cab49d757a7f86a29d1ba307539
noomesk <aplazasn@unal.edu.co>
2026-08-20 12:01:01 -0500

    Pruebas de config desde atributo HTML a js

---
+++ introduccion/main.js
@@ -0,0 +1,8 @@
+import './components/weather-dashboard.js';
+
+document.querySelector('#app').innerHTML = ` 
+  <weather-dashboard></weather-dashboard>
+`;
+
+//│   registra: "weather-dashboard" → WeatherDashboard
+//<weather-dashboard></weather-dashboard> 
